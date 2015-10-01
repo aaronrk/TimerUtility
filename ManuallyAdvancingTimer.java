e8936428843c101ddd784bf049786b9f82e7ce1b
aaronrk <aaronrk@users.noreply.github.com>
2015-10-01 10:49:39 -0700

    Update ManuallyAdvancingTimer.java

--- ManuallyAdvancingTimer.java
+++ ManuallyAdvancingTimer.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package project.somepackagename;
 
 import java.util.Arrays;
 import java.util.Date;
